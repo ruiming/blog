ac04ea0a6419b15673412fda4c566fe5deb388bf
Ruiming <ruiming.zhuang@gmail.com>
2016-03-08 22:12:34 +0800

    fix archive

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Suin\\RSSWriter' => array($vendorDir . '/suin/php-rss-writer/src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
