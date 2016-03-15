00b24f616d339c30fa551512cafa339eb4ca9d82
Ruiming <ruiming.zhuang@gmail.com>
2016-03-15 19:57:04 +0800

    fix md css - 2

--- resources/views/blog/layout.blade.php
+++ resources/views/blog/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>{{config('blog.title')}}</title>
-    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/style.css?v=2') }}" rel="stylesheet">
     <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
