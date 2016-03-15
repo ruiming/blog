3525b667512b2155172e6ace337cd025f6f764e0
Ruiming <ruiming.zhuang@gmail.com>
2016-03-15 19:36:52 +0800

    fix md css

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,12 +1,14 @@
 @extends('blog.layout')
 @section('title', 'Page Title')
 @section('content')
-    <div class="blog">
-        <h1>{{ $post->title }}</h1>
-        <h5>发布于 {{ $post->created_at }}</h5>
-        <hr>
-        <p>{!! $post->content !!}</p>
-        <hr>
+    <div class="post">
+        <h2 class="post-title">{{ $post->title }}</h2>
+        <div class="post-content">
+            <p>
+                {!! $post->content !!}
+            </p>
+            <small>发布于 {{ $post->created_at }}</small>
+        </div>
         @include('disqus.disqus')
     </div>
 @endsection
